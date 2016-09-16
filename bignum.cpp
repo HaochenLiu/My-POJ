2f60b5eeee10c2a3ffe104456edfc55aff600e18
Haochen Liu <liuhaochen1221@hotmail.com>
2016-09-15 23:22:17 -0700

    Update bignum.cpp

--- bignum.cpp
+++ bignum.cpp
@@ -912,7 +912,6 @@ int c2num(char c) {
     return (c - 'a' + 36);
 }
 
-
 /*
  *===========================================
  *                  Test
@@ -920,84 +919,84 @@ int c2num(char c) {
  */
 
 bool testZeroEqualsToZero() {
-	bignum a = 0;
+    bignum a = 0;
     return (a == 0);
 }
 
 bool testPositiveNumNotEqualsToZero_1() {
-	bignum a = 1;
+    bignum a = 1;
     return !(a == 0);
 }
 
 bool testPositiveNumNotEqualsToZero_2() {
-	bignum a = 100000000;
+    bignum a = 100000000;
     return !(a == 0);
 }
 
 bool testNegativeNumNotEqualsToZero_1() {
-	bignum a = -1;
+    bignum a = -1;
     return !(a == 0);
 }
 
 bool testNegativeNumNotEqualsToZero_2() {
-	bignum a = -100000000;
+    bignum a = -100000000;
     return !(a == 0);
 }
 
 bool testZeroNotGreaterThanZero() {
-	bignum a = 0;
+    bignum a = 0;
     return !(a > 0);
 }
 
 bool testPositiveNumGreaterThanZero_1() {
-	bignum a = 1;
+    bignum a = 1;
     return (a > 0);
 }
 
 bool testPositiveNumGreaterThanZero_2() {
-	bignum a = 100000000;
+    bignum a = 100000000;
     return (a > 0);
 }
 
 void test() {
-	if(!testZeroEqualsToZero()) {
-		cout<<"testZeroEqualsToZero failed."<<endl;
-		return;
-	}
-	if(!testPositiveNumNotEqualsToZero_1()) {
-		cout<<"testPositiveNumNotEqualsToZero_1 failed."<<endl;
-		return;
-	}
-	if(!testPositiveNumNotEqualsToZero_2()) {
-		cout<<"testPositiveNumNotEqualsToZero_2 failed."<<endl;
-		return;
-	}
-	if(!testNegativeNumNotEqualsToZero_1()) {
-		cout<<"testNegativeNumNotEqualsToZero_1 failed."<<endl;
-		return;
-	}
-	if(!testNegativeNumNotEqualsToZero_2()) {
-		cout<<"testNegativeNumNotEqualsToZero_2 failed."<<endl;
-		return;
-	}
-	if(!testZeroNotGreaterThanZero()) {
-		cout<<"testZeroNotGreaterThanZero failed."<<endl;
-		return;
-	}
-	if(!testPositiveNumGreaterThanZero_1()) {
-		cout<<"testPositiveNumGreaterThanZero_1 failed."<<endl;
-		return;
-	}
-	if(!testPositiveNumGreaterThanZero_2()) {
-		cout<<"testPositiveNumGreaterThanZero_2 failed."<<endl;
-		return;
-	}
-	cout<<"Test passed."<<endl;
+    if(!testZeroEqualsToZero()) {
+        cout<<"testZeroEqualsToZero failed."<<endl;
+        return;
+    }
+    if(!testPositiveNumNotEqualsToZero_1()) {
+        cout<<"testPositiveNumNotEqualsToZero_1 failed."<<endl;
+        return;
+    }
+    if(!testPositiveNumNotEqualsToZero_2()) {
+        cout<<"testPositiveNumNotEqualsToZero_2 failed."<<endl;
+        return;
+    }
+    if(!testNegativeNumNotEqualsToZero_1()) {
+        cout<<"testNegativeNumNotEqualsToZero_1 failed."<<endl;
+        return;
+    }
+    if(!testNegativeNumNotEqualsToZero_2()) {
+        cout<<"testNegativeNumNotEqualsToZero_2 failed."<<endl;
+        return;
+    }
+    if(!testZeroNotGreaterThanZero()) {
+        cout<<"testZeroNotGreaterThanZero failed."<<endl;
+        return;
+    }
+    if(!testPositiveNumGreaterThanZero_1()) {
+        cout<<"testPositiveNumGreaterThanZero_1 failed."<<endl;
+        return;
+    }
+    if(!testPositiveNumGreaterThanZero_2()) {
+        cout<<"testPositiveNumGreaterThanZero_2 failed."<<endl;
+        return;
+    }
+    cout<<"Test passed."<<endl;
 }
 
 int main() {
-	test();
-	getchar();
+    test();
+    getchar();
 
     return 0; 
 }
